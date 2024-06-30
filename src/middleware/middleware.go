3ba3a7d6fc230a49a1c4bdc06e8ea39309542706
Tieres Bard <tieresbard@hotmail.com>
2024-06-30 10:38:53 -0300

    middleware para tratativas de autenticação utilizando token

--- src/middleware/middleware.go
+++ src/middleware/middleware.go
@@ -1,22 +1,28 @@
 package middleware
 
 import (
+	"api/src/auth"
+	"api/src/response"
 	"fmt"
 	"net/http"
 )
 
 // Função que apresenta as informações do request
-func Logger(next http.HandlerFunc) http.HandlerFunc {
+func Logger(nextFunc http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Making request...\n Método %s \n URI %s \n Host %s \n ", r.Method, r.RequestURI, r.Host)
-		next(w, r)
+		nextFunc(w, r)
 	}
 }
 
 // Verifica se usuario está autenticado para acessar a rota que exige esse requisito
-func Authentication(next http.HandlerFunc) http.HandlerFunc {
+func Authentication(nextFunc http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if err := auth.ValidateToken(r); err != nil {
+			response.Err(w, http.StatusUnauthorized, err)
+			return
+		}
 		fmt.Println("Validating...")
-		next(w, r)
+		nextFunc(w, r)
 	}
 }
