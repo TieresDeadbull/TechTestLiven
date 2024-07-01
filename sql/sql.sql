4e56101fa6520f32aa7ca29f53aff2ab436e89b2
Tieres Bard <tieresbard@hotmail.com>
2024-07-01 02:22:37 -0300

    Registrando comandos usados para a geraçao do banco de dados

--- sql/sql.sql
+++ sql/sql.sql
@@ -10,4 +10,22 @@ CREATE TABLE users(
     passphrase varchar(100) not null,
     phoneNumber varchar(16) not null unique,
     createdAt timestamp default current_timestamp()
-) ENGINE=INNODB;
+) ;
+
+ CREATE TABLE addresses (
+    id INT auto_increment PRIMARY KEY,
+    street VARCHAR(255) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    zipcode VARCHAR(20) NOT NULL,
+    createdAt timestamp default current_timestamp()
+)ENGINE=INNODB;
+
+CREATE TABLE user_addresses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    address_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (address_id) REFERENCES addresses(id),
+    UNIQUE KEY user_address_unique (user_id, address_id)
+)ENGINE=INNODB;
